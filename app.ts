96574e1d7736a4a45364d456d8304da52dbd2dcc
Painkiller-69 <128809931+Painkiller-69@users.noreply.github.com>
2023-06-01 21:50:31 +0530

    app.ts

--- app.ts
+++ app.ts
@@ -1,72 +1,83 @@
 interface Product {
-    title: string;
-    image: string;
-    price: number;
-    description: string;
+  title: string;
+  image: string;
+  price: number;
+  description: string;
+}
+
+const products: Product[] = [
+  {
+    title: "Product1",
+    image: "https://picsum.photos/200",
+    price: 380,
+    description: "first product"
+  },
+  {
+    title: "Product2",
+    image: "https://picsum.photos/200",
+    price: 980,
+    description: "second product"
+  },
+  {
+    title: "Product3",
+    image: "https://picsum.photos/200",
+    price: 1200,
+    description: "third product"
   }
-  
-  const products: Product[] = [
-    {
-      "title": "Product1",
-      "image": "https://picsum.photos/200",
-      "price": 380,
-      "description": "first product"
-    },
-    {
-      "title": "Product2",
-      "image": "https://picsum.photos/200",
-      "price": 980,
-      "description": "second product"
-    },
-    {
-      "title": "Product3",
-      "image": "https://picsum.photos/200",
-      "price": 1200,
-      "description": "third product"
-    }
-  ];
-  
-  // User Story 1: Display all the products in UI
-  const productList = document.getElementById("productList");
-  products.forEach((product) => {
-    const productElement = document.createElement("div");
-    productElement.innerHTML = `
-      <img src="${product.image}" alt="${product.title}">
-      <h3>${product.title}</h3>
-      <p>Price: ${product.price}</p>
-      <p>Description: ${product.description}</p>
-    `;
-    productList.appendChild(productElement);
-  });
-  
-  // User Story 2: Display all the product names in select (drop-down)
-  const productSelect = document.getElementById("productSelect") as HTMLSelectElement;
-  products.forEach((product) => {
-    const option = document.createElement("option");
-    option.value = product.title;
-    option.textContent = product.title;
+];
+
+// User Story 1: Display all products
+const productListDiv: HTMLElement | null = document.getElementById("productList");
+products.forEach((product: Product) => {
+  const productDiv: HTMLDivElement = document.createElement("div");
+  productDiv.innerHTML = `
+    <h3>${product.title}</h3>
+    <img src="${product.image}" alt="${product.title}">
+    <p>Price: $${product.price}</p>
+    <p>Description: ${product.description}</p>
+  `;
+  if (productListDiv) {
+    productListDiv.appendChild(productDiv);
+  }
+});
+
+// User Story 2: Display product names in select dropdown
+const productSelect: HTMLSelectElement | null = document.getElementById("productSelect") as HTMLSelectElement;
+products.forEach((product: Product, index: number) => {
+  const option: HTMLOptionElement = document.createElement("option");
+  option.value = index.toString();
+  option.text = product.title;
+  if (productSelect) {
     productSelect.appendChild(option);
-  });
-  
-  // User Story 3: On selecting any product from the list, display the product details of the selected product only.
-  function displayProductDetails() {
-    const selectedProduct = productSelect.value;
-    const productDetails = document.getElementById("productDetails");
-    productDetails.innerHTML = "";
-  
-    if (selectedProduct) {
-      const selectedProductData = products.find(
-        (product) => product.title === selectedProduct
-      );
-  
-      const productElement = document.createElement("div");
-      productElement.innerHTML = `
-        <img src="${selectedProductData.image}" alt="${selectedProductData.title}">
-        <h3>${selectedProductData.title}</h3>
-        <p>Price: ${selectedProductData.price}</p>
-        <p>Description: ${selectedProductData.description}</p>
-      `;
-      productDetails.appendChild(productElement);
-    }
   }
-  
+});
+
+
+// User Story 3: Display selected product details
+function showProductDetails(): void {
+  const productSelect: HTMLSelectElement | null = document.getElementById("productSelect") as HTMLSelectElement;
+  const selectedIndex: number = parseInt(productSelect?.value || "0");
+  const selectedProduct: Product = products[selectedIndex];
+  const productDetailsDiv: HTMLElement | null = document.getElementById("productDetails");
+  const productListDiv: HTMLElement | null = document.getElementById("productList");
+
+  // Clear existing product details
+  if (productDetailsDiv) {
+    productDetailsDiv.innerHTML = "";
+  }
+
+  // Hide product list
+  if (productListDiv) {
+    productListDiv.style.display = "none";
+  }
+
+  // Render selected product details
+  if (productDetailsDiv) {
+    productDetailsDiv.innerHTML = `
+      <h3>${selectedProduct.title}</h3>
+      <img src="${selectedProduct.image}" alt="${selectedProduct.title}">
+      <p>Price: $${selectedProduct.price}</p>
+      <p>Description: ${selectedProduct.description}</p>
+    `;
+  }
+}
